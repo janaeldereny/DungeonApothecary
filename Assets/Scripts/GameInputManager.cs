6d9d562e9fcad99a7599513feafce62fc3bc85a2
janaeldereny <janaeldereny@gmail.com>
2026-07-17 15:01:50 +0300

    movement

--- Assets/Scripts/GameInputManager.cs
+++ Assets/Scripts/GameInputManager.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using System;
+[DefaultExecutionOrder(-1)]
 
 public class GameInputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameInputManager Instance { get; private set; }
+
+    NewActions inputActions;
+    public event Action OnInteract;
+    public event Action OnDrop;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        inputActions = new NewActions();
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        inputActions.Enable();
+        inputActions.Player.Interact.performed += Interact_performed;
+        inputActions.Player.Drop.performed += Drop_performed;
     }
+
+    void OnDisable()
+    {
+        inputActions.Player.Interact.performed -= Interact_performed;
+        inputActions.Player.Drop.performed -= Drop_performed;
+        inputActions.Disable();
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnInteract?.Invoke();
+        }
+    }
+
+    private void Drop_performed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnDrop?.Invoke();
+        }
+    }
+
+    public Vector2 GetMovement()
+    {
+        return inputActions.Player.Move.ReadValue<Vector2>().normalized;
+    }
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float moveSpeed = 2.7f;
+    Vector2 moveInput;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        flipSprite();
+
+        //  transform.position += (Vector3)moveInput * moveSpeed * Time.deltaTime;
     }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+    }
+
+    void HandleMovement()
+    {
+        moveInput = GameInputManager.Instance.GetMovement();
+        moveInput.Normalize();
+       
+    }
+    
+
+
+void flipSprite()
+{
+    if (moveInput.x > 0)
+    {
+        spriteRenderer.flipX = true; 
+    }
+    else if (moveInput.x < 0)
+    {
+        spriteRenderer.flipX = false; 
+    }
+}
+
 }
