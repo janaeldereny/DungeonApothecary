22195e1cbaf2f35df0d8b651509f19b5ba817fca
janaeldereny <janaeldereny@gmail.com>
2026-07-27 12:33:40 +0300

    Player Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleMovement();
-        flipSprite();
+        //flipSprite();
 
         //  transform.position += (Vector3)moveInput * moveSpeed * Time.deltaTime;
     }
@@ -29,6 +31,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
+        bool isMoving = moveInput != Vector2.zero;
+
+        animator.SetBool("isWalking",isMoving);
+
+        if (isMoving)
+        {
+             animator.SetFloat("InputX" , moveInput.x );
+            animator.SetFloat("InputY" , moveInput.y );
+            //animator.SetBool("isWalking" , false);
+            animator.SetFloat("LastInputX" , moveInput.x );
+            animator.SetFloat("LastInputY" , moveInput.y );
+        }
+
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
     }
 
@@ -39,16 +54,16 @@ public class PlayerMovement : MonoBehaviour
        
     }
     
-void flipSprite()
-{
-    if (moveInput.x > 0)
-    {
-        spriteRenderer.flipX = true; 
-    }
-    else if (moveInput.x < 0)
-    {
-        spriteRenderer.flipX = false; 
-    }
-}
+// void flipSprite()
+// {
+//     if (moveInput.x > 0)
+//     {
+//         spriteRenderer.flipX = true; 
+//     }
+//     else if (moveInput.x < 0)
+//     {
+//         spriteRenderer.flipX = false; 
+//     }
+// }
 
 }
