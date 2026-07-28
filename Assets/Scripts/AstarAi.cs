288c658b07efe92ecc857fd59d5a5e04d4c43dfa
janaeldereny <janaeldereny@gmail.com>
2026-07-29 01:00:51 +0300

    init SpawnManager

--- Assets/Scripts/AstarAi.cs
+++ Assets/Scripts/AstarAi.cs
@@ -7,6 +7,7 @@ public class AstarAI : MonoBehaviour
 
     private Seeker seeker;
     private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
 
     public Path path;
 
@@ -28,10 +29,28 @@ public class AstarAI : MonoBehaviour
 
     public void Start()
     {
+        
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponentInChildren<Animator>();
+        Debug.Log(animator.gameObject.name);
+        //animator= GetComponent<Animator>();
 
         InvokeRepeating(nameof(UpdatePath), 0f, pathUpdateInterval);
+
+   
+        //   if (targetPosition == null)
+        // {
+        //     GameObject player = GameObject.FindGameObjectWithTag("Player");
+        //     if (player != null)
+        //     {
+        //         targetPosition = player.transform;
+        //     }
+        //     else
+        //     {
+        //         Debug.LogWarning("Player not found! Make sure the Player GameObject has the 'Player' tag.");
+        //     }
+        // }
     }
 
     void UpdatePath()
@@ -96,6 +115,32 @@ public class AstarAI : MonoBehaviour
         Vector2 dir = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Vector2 velocity = dir * speed * speedFactor;
 
+        if(!reachedEndOfPath && velocity.sqrMagnitude > 0.01f)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+            SetWalkingAnimation(true,dir);
+        }
+        else
+        {
+            SetWalkingAnimation(false,Vector2.zero);
+        }
+
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
     }
+
+    private void SetWalkingAnimation(bool isWalking, Vector2 direction)
+    {
+        if (animator== null) return;
+        animator.SetBool ("isWalking",isWalking);
+
+        if (isWalking)
+        {
+            animator.SetFloat("moveX", direction.x);
+            animator.SetFloat("moveY", direction.y);
+
+             animator.SetFloat("lastmoveX", direction.x);
+            animator.SetFloat("lastmoveY", direction.y);
+        }
+    }
+
 }
